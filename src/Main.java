35eb4280199692a03ca4ee8dd3b6a43e7c2b5a87
wojownikgier <kacperistelskixd@gmail.com>
2024-09-11 11:40:57 +0200

    losowe liczby do 100

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //tablicza
+        // tablicza musi miec z gory okreslony rozmiar nie mozna go pusniej znmienic  alementy moga byc cyfty  proszte i złozone
+        // tablicza z 6 wartoscami losowymi z zakresu 1 do 100
+        int[] tabliczalosowa = new int[6];
+        for (int i = 0; i < tabliczalosowa.length; i++) {
+            tabliczalosowa[i] = (int) (Math.random()*100 + 1);
+        }
+        //wipisana tablicza
+        for (int element:tabliczalosowa){
+            System.out.println(element+",");
+        }
     }
-}
+    }
+
+
+
