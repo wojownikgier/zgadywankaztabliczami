eeb48556b207d3183795016f285602f8a1c2d316
wojownikgier <kacperistelskixd@gmail.com>
2024-09-11 12:25:23 +0200

    losowe

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //tablicza
-        // tablicza musi miec z gory okreslony rozmiar nie mozna go pusniej znmienic  alementy moga byc cyfty  proszte i złozone
-        // tablicza z 6 wartoscami losowymi z zakresu 1 do 100
-        int[] tabliczalosowa = new int[6];
-        for (int i = 0; i < tabliczalosowa.length; i++) {
-            tabliczalosowa[i] = (int) (Math.random()*100 + 1);
+
+        System.out.println("Hello world!");
+
+        //tablica
+        //musi mieć z góry określony rozmiar
+        //nie można go później zmienić
+        //elementem tablicy mogą byc typy proste i złożone
+        //tablica 6 elementowa z wartościami losowym z zakresu 1,100
+        int [] tablicaLosowych = new int[6];
+        for (int i = 0; i < tablicaLosowych.length; i++) {
+            tablicaLosowych[i] = (int) (Math.random()*100+1);
         }
-        //wipisana tablicza
-        for (int element:tabliczalosowa){
-            System.out.println(element+",");
+        //wypisywanie elementów tablicy
+        for (int element:tablicaLosowych) {
+            System.out.print(element+", ");
         }
+        //kolekcje, elemnty typami złożonymi
+        //nie muszą mieć zdefiniowanego rozmiaru
+        //rozmiar w trakcie może zmieniać
+        //listy List, zbiory Set, mapy
+
+        //wstawianie liczb z klawiatury do kolekcji
+        ArrayList<Integer> listaLiczbWpisanych = new ArrayList<>();
+        System.out.println("podaj 6 liczb");
+        Scanner klawiatura = new Scanner(System.in);
+        for (int i = 0; i < 6; i++) {
+            int liczba = klawiatura.nextInt();
+            listaLiczbWpisanych.add(liczba);
+        }
+        System.out.println("Wstawiono liczby:");
+        for (Integer liczbaWpisana:listaLiczbWpisanych) {
+            System.out.println(liczbaWpisana);
+        }
+        //wypisywanie inaczej
+        for (int i = 0; i < listaLiczbWpisanych.size(); i++) {
+            System.out.println(listaLiczbWpisanych.get(i));
+        }
+
+
+
+
     }
-    }
-
-
-
+}
